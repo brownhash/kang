debc0bd040f4a8e27fd878881675c0d4a646c110
Harshit Sharma <sharma1612harshit@gmail.com>
2021-09-16 00:40:55 +0530

    shell sessions package

---
+++ internal/session/session.go
@@ -0,0 +1,36 @@
+package session
+
+import (
+	"fmt"
+	"time"
+	"os/user"
+
+	"github.com/brownhash/golog"
+	"github.com/gofrs/uuid"
+)
+
+func GenerateSession() Session {
+	uid, err := uuid.NewV4()
+	if err != nil {
+		golog.Error(err.Error())
+	}
+
+	user, err := user.Current()
+	if err != nil {
+		golog.Error(err.Error())
+	}
+
+	session := Session{
+		Id: fmt.Sprintf("%v", uid),
+		Started: time.Now().Local(),
+		User: user.Username,
+	}
+
+	return session
+}
+
+type Session struct {
+	Id 			string
+	Started 	time.Time
+	User		string 		
+}
