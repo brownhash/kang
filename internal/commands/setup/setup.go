7d8811ece4cfc879604d6fdba66c1e67429ab516
Harshit Sharma <sharma1612harshit@gmail.com>
2021-09-16 02:58:18 +0530

    initiate setup pkg

---
+++ internal/commands/setup/setup.go
@@ -0,0 +1,17 @@
+package setup
+
+import (
+)
+
+func Run(args []string) int {
+
+	return 0
+}
+
+func Help() string {
+	return "\n\tSets up required structure and dependencies for self.\n"
+}
+
+func Synopsis() string {
+	return "Self setup."
+}
