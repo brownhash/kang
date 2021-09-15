0940ac0bc62be54b25344c9007c49a6825f3291b
Harshit Sharma <sharma1612harshit@gmail.com>
2021-09-16 03:16:11 +0530

    base setup pkg

--- internal/commands/setup/setup.go
+++ internal/commands/setup/setup.go
@@ -1,9 +1,37 @@
 package setup
 
 import (
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/brownhash/golog"
 )
 
 func Run(args []string) int {
+	setupPath := os.Getenv("HOME")
+
+	if len(args) > 0 {
+		golog.Error(fmt.Sprintf("Unrecognised arguments %v. setup requires no argument", args))
+		return 1
+	}
+
+	setupPath = path.Join(setupPath, ".sessionTerraform")
+
+	_, err := os.Stat(setupPath)
+	if err == nil {
+		golog.Warn(fmt.Sprintf("Set up already done at %v", setupPath))
+		return 0
+	}
+
+	err = os.Mkdir(setupPath, 0755)
+
+	if err != nil {
+		golog.Error(err)
+		return 1
+	}
+
+	golog.Success(fmt.Sprintf("Set up complete at %v", setupPath))
 
 	return 0
 }
