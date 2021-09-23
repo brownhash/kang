34e4d70605ceb888e7ec9211e0c6bfd2a521fd7d
Harshit Sharma <sharma1612harshit@gmail.com>
2021-09-23 21:30:52 +0530

    add todos

--- config/globals.go
+++ config/globals.go
@@ -9,5 +9,5 @@ var TerraformRegistryUrl string = "releases.hashicorp.com/terraform"
 var SetupPath string = path.Join(os.Getenv("HOME"), ".sessionTerraform")
 var DefaultDirPermission os.FileMode = 755
 var TempExtension string = ".tmp"
-var OsType string = "darwin"
-var OsArchitecture string = "amd64"
+var OsType string = "darwin" // TODO: Find for system
+var OsArchitecture string = "amd64" // TODO: Find for system
