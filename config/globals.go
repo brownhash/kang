45745c2c999c93a7db6a5f77768067b663e04627
Harshit Sharma <sharma1612harshit@gmail.com>
2021-09-24 00:24:53 +0530

    fetch system os name and arch

--- config/globals.go
+++ config/globals.go
@@ -3,11 +3,12 @@ package config
 import (
 	"os"
 	"path"
+	"runtime"
 )
 
 var TerraformRegistryUrl string = "releases.hashicorp.com/terraform"
 var SetupPath string = path.Join(os.Getenv("HOME"), ".sessionTerraform")
 var DefaultDirPermission os.FileMode = 755
 var TempExtension string = ".tmp"
-var OsType string = "darwin" // TODO: Find for system
-var OsArchitecture string = "amd64" // TODO: Find for system
+var OsType string = runtime.GOOS
+var OsArchitecture string = runtime.GOARCH
