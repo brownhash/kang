7bd93fa67c405c8aa98146218165bdcc8157f0f5
Harshit Sharma <sharma1612harshit@gmail.com>
2021-10-11 21:19:52 +0530

    fetch command

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 
 	"github.com/brownhash/golog"
-	"github.com/brownhash/kang/internal/commands"
+	"github.com/brownhash/kang/internal/command"
 	"github.com/brownhash/kang/internal/core"
 	"github.com/mitchellh/cli"
 )
@@ -27,19 +27,9 @@ func main() {
 
 	golog.Debug(fmt.Sprintf("%s successfully initated", appName))
 
-	commandName := "--help"
-	if len(os.Args) > 1 {
-		commandName = os.Args[1]
-	}
-	
-	args := []string{}
-	if len(os.Args) > 2 {
-		args = os.Args[2:]
-	}
-
 	c := cli.NewCLI(appName, appVersion)
 	c.Args = os.Args[1:]
-	c.Commands = commands.CommandCatalog(commandName, args)
+	c.Commands = command.CommandCatalog()
 
 	exitStatus, err := c.Run()
 	if err != nil {
